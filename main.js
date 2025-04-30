3f706afaf49dd7d47cf375b684a7993782c6d45d
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-04-30 09:44:02 +0200

    Er nu ved Javascript delen til FAQ + har rettet lidt i oplevelser.

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const offersContainer = document.getElementById("offers");
-    const resetButton = document.getElementById("reset");
+  const offersContainer = document.getElementById("offers");
+  const resetButton = document.getElementById("reset");
+  const acc = document.querySelectorAll(".accordion");
 
-    offersContainer.addEventListener("click", function (event) {
-        if (event.target.classList.contains("redeem-btn")) {
-            const offer = event.target.parentElement;
-            offer.style.opacity = '0';
-            setTimeout(() => offer.remove(), 300);
-        }
+  offersContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("redeem-btn")) {
+      const offer = event.target.parentElement;
+      offer.style.opacity = '0';
+      setTimeout(() => offer.remove(), 300);
+    }
+  });
+
+  acc.forEach(btn => {
+    btn.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const panel = this.nextElementSibling;
+
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
     });
 
-    resetButton.addEventListener("click", function () {
-        offersContainer.innerHTML = `
+  });
+
+  resetButton.addEventListener("click", function () {
+    offersContainer.innerHTML = `
         <div class="offer">
           <span>Billet til Korsbæk Bio</span>
           <button class="redeem-btn">Indløs</button>
@@ -25,5 +40,6 @@ document.addEventListener("DOMContentLoaded", function () {
           <button class="redeem-btn">Indløs</button>
         </div>
       `;
-    });
+  });
 });
+
